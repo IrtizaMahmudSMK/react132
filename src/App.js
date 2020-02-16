7152eb907af3f5e5da4f50c79d118a331dbe3df6
Irtiza Mahmud <irtiza.apploye@gmail.com>
2020-02-16 09:55:45 +0600

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ContactList extends React.Component {
+  render() {
+    const people = this.props.contacts
+
+    return <ol>
+      {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+         <ContactList
+          contacts= {[
+            { name: "Jayeed Ibn Harisa(Ra:)" },
+            { name: "Jafor Ibn Abu Talib(Ra:)" },
+            { name: "Abdullah Ibn Rawaha(Ra:)" }
+          ]}
+        />
+        <ContactList  contacts = { [
+           { name: "Saad Ibne Muaaj(Ra:)" },
+           { name: "Abu Ubaidah Ibn Zarrah(Ra:)" },
+           { name: "Abdur Rahman Ibn Aauf" }
+        ]}/> 
+      </div>
+    );
+  }
 }
 
 export default App;
